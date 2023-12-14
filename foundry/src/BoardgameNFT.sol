4abd0121f1f070aa42656a3ab8687a1350448eeb
KUN LI <likun9452@gmail.com>
2023-12-13 20:57:06 -0800

    Add uri to bgt

--- foundry/src/BoardgameNFT.sol
+++ foundry/src/BoardgameNFT.sol
@@ -7,24 +7,22 @@ contract BoardgameNFT is ERC721 {
     uint256 private _tokenIdCounter;
     mapping(uint256 => string) private _tokenURIs;
     mapping(address => uint256[]) private _ownedTokens;
+    string private baseTokenURI;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        baseTokenURI = "https://firebasestorage.googleapis.com/v0/b/planar-hangout-366503.appspot.com/o/bgt_meta%2F";
+    }
 
     function mint(address to, string memory uri) public {
+        _tokenIdCounter++;
         uint256 tokenId = _tokenIdCounter;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         _ownedTokens[to].push(tokenId);
-        _tokenIdCounter++;
     }
 
     function _setTokenURI(uint256 tokenId, string memory uri) internal {
         _tokenURIs[tokenId] = uri;
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return _tokenURIs[tokenId];
     }
 
     function tokensOfOwner(address owner) external view returns (uint256[] memory) {
@@ -55,5 +53,10 @@ contract BoardgameNFT is ERC721 {
             _ownedTokens[from][tokenIndex] = _ownedTokens[from][lastTokenIndex];
         }
         _ownedTokens[from].pop();
+    }
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        return string(abi.encodePacked(baseTokenURI, Strings.toString(tokenId), ".json?alt=media"));
     }
 }
